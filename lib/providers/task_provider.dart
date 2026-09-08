dec5fec690cf6a1b796e5630789ae6c9d208dc82
Justin B. Lauricio <lauriciolau902@gmail.com>
2026-09-09 03:44:48 +0800

    feat: connect TaskProvider to Firestore stream

--- lib/providers/task_provider.dart
+++ lib/providers/task_provider.dart
@@ -3,3 +3,22 @@ class TaskProvider extends ChangeNotifier {
 
   List<Task> _allTasks = [];
 }
+
+StreamSubscription<List<Task>>? _subscription;
+
+TaskProvider() {
+  _listenToTasks();
+}
+
+void _listenToTasks() {
+  _subscription = _taskService.getTasks().listen((tasks) {
+    _allTasks = tasks;
+    notifyListeners();
+  });
+}
+
+@override
+void dispose() {
+  _subscription?.cancel();
+  super.dispose();
+}
